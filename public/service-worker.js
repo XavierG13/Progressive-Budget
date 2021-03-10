0c0487b0226a53449b42626852e48a675e7385cc
Xavier Guzman <guzmanxavi112@gmail.com>
2021-03-09 21:17:49 -0600

    update to service worker and server.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,6 +6,8 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 const STATIC_CACHE = "static-cache-v1";
