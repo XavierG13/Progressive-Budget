a71daebead8c7740ddabfb65a2f6be7fb1e21488
Xavier Guzman <guzmanxavi112@gmail.com>
2021-03-09 21:44:58 -0600

    changed mongoose to Budget

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/cryptic-lake", {
+mongoose.connect("mongodb://localhost/Budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
